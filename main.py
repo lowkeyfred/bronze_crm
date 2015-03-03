28eda8c9759db50ad8be451ec517b1a36f16c5d4
caimmy <caimmy@hotmail.com>
2015-03-04 00:34:36 +0800

    config system by variable

---
+++ main.py
@@ -0,0 +1,26 @@
+# _*_ coding:utf-8 _*_
+
+__author__ = 'caimiao'
+__date__ = '2015-03-03'
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+from flask import Flask
+from config import global_settings
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    print(app.config)
+    return '阿三服阿斯顿服'
+
+@app.errorhandler(404)
+def test_404(error):
+    print(app.config)
+    return '<h1>404 occur</h1><br />%s' % (str(error)), 404
+
+app.config.from_object(global_settings)
+app.run(host='0.0.0.0', port=5000)
